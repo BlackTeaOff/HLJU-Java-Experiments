d8b03a45f50515e534a3b9f82aff91f0419d9abe
BlackTea <1571106964@qq.com>
2025-06-19 13:06:22 +0800

    增加注释

--- exp_6/src/Pet.java
+++ exp_6/src/Pet.java
@@ -2,3 +2,10 @@ public interface Pet { // 对事物的局部行为进行抽象;允许方法接�
     String getName(); // 接口的核心作用是定义一个规范或契约
     int getAge();
 }
+
+// 作用
+
+// 定义一个规范(标准): 即所有实现它的类(implements)(PetImpl->Cat&Dog)都必须实现上述方法
+// 不关心方法如何实现(抽象)，只关心它们必须存在(建立了一个标准)
+
+// 实现多态: 在PetShop类中，add和delete方法可以传入实现了Pet接口的实例，即可以传入多种动物
