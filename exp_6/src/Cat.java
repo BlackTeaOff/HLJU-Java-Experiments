770abfb353c2022371fbebb9efcee593ce5e80e4
BlackTea <1571106964@qq.com>
2025-06-19 13:07:44 +0800

    直接调用父类构造器，删掉多余this.name = name.....

--- exp_6/src/Cat.java
+++ exp_6/src/Cat.java
@@ -1,7 +1,5 @@
 public class Cat extends PetImpl { // Cat Is-A 宠物
     Cat(String name, int age) {
-        super(name, age);
-        this.name = name;
-        this.age = age;
+        super(name, age); // 直接调用父类构造器
     }
 }
